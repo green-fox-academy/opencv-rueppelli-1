2421d049bc4bb2d2499535d0b5901e8dc884ebe5
EXECNOIR <gump891202@gmail.com>
2019-01-29 11:07:02 +0100

    OP-1 Project Repository Setup

---
+++ files/initial.cpp
@@ -0,0 +1,11 @@
+//
+// Created by EXEC_NOIR on 1/29/2019.
+//
+
+#include "initial.h"
+
+
+int initialFunction(int a, int b)
+{
+    return a + b;
+}
---
+++ gtest/unit_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by EXEC_NOIR on 1/29/2019.
+//
+
+#include <gtest/gtest.h>
+#include "initial.h"
+
+TEST(test_check, test_cuccok){
+    EXPECT_EQ(initialFunction(1, 1), 2);
+}
