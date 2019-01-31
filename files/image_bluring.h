097d74195fc73b60e9c7236e3e960ffc81105ca5
EXECNOIR <43448395+EXECNOIR@users.noreply.github.com>
2019-01-31 12:26:00 +0100

    Update image_bluring.h
    deleted extern variables.

--- files/image_bluring.h
+++ files/image_bluring.h
@@ -1,11 +1,6 @@
 #ifndef OPENCV1_PROJECT_IMAGE_BLURING_H
 #define OPENCV1_PROJECT_IMAGE_BLURING_H
 
-extern cv::Mat img;
-extern cv::Mat smoothed_img;
-extern int v_median;
-extern int v_gaussian;
-
 void gaussian(int, void *);
 void median(int, void *);
 void bluring_image();
