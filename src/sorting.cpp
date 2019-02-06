d534e3d923a537a9c511dbac010f07be340bd24d
EXECNOIR <43448395+EXECNOIR@users.noreply.github.com>
2019-02-06 09:51:53 +0100

    Update sorting.cpp

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -143,3 +143,80 @@ void sortArray(int* array, int low, int high, int &counter, int order)
     }
 }
 
+int *insertion_sort(int *array, int size, int command)
+{
+    auto start = cv::getTickCount();
+    int counter = 0;
+    if (command == 1) {
+        for (int i = 1; i < size; i++) {
+            int temp_var = array[i];
+            int j = i - 1;
+
+            while (array[j] > temp_var && j >= 0) {
+                array[j + 1] = array[j];
+                j--;
+                counter++;
+            }
+            array[j + 1] = temp_var;
+        }
+    } else if (command == 0) {
+        for (int i = 1; i < size; i++) {
+            int temp_var = array[i];
+            int j = i - 1;
+
+            while (array[j] < temp_var && j >= 0) {
+                array[j + 1] = array[j];
+                j--;
+                counter++;
+            }
+            array[j + 1] = temp_var;
+        }
+    }
+
+    auto finish = cv::getTickCount();
+    double duration = (finish - start) / cv::getTickFrequency();
+    std::cout << "Time taken by function: " << duration << " microseconds" << std::endl;
+
+    std::cout << "It was " << counter << " steps to the result." << std::endl;
+    return array;
+}
+
+std::vector<int> insertion_sort(std::vector<int> &vector, int size, int command)
+{
+    auto start = cv::getTickCount();
+    int counter = 0;
+
+    if (command == 1) {
+        for (int i = 1; i < size; i++) {
+            int temp_var = vector[i];
+            int j = i - 1;
+
+            while (vector[j] > temp_var && j >= 0) {
+                vector[j + 1] = vector[j];
+                j--;
+                counter++;
+            }
+            vector[j + 1] = temp_var;
+        }
+    } else if (command == 0) {
+        for (int i = 1; i < size; i++) {
+            int temp_var = vector[i];
+            int j = i - 1;
+
+            while (vector[j] < temp_var && j >= 0) {
+                vector[j + 1] = vector[j];
+                j--;
+                counter++;
+            }
+            vector[j + 1] = temp_var;
+        }
+    }
+
+    auto finish = cv::getTickCount();
+    double duration = (finish - start) / cv::getTickFrequency();
+    std::cout << "Time taken by function: " << duration << " microseconds" << std::endl;
+
+    std::cout << "It was " << counter << " steps to the result." << std::endl;
+    return vector;
+} 
+
