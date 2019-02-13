e4cf741de5a73dcb6d3c24054a44b32b6181fece
KisG93 <kisg93@gmail.com>
2019-02-13 15:23:07 +0100

    insertionTest

--- gtest/unit_test.cpp
+++ gtest/unit_test.cpp
@@ -35,15 +35,15 @@ TEST(test_check, overlaying_circle)
 TEST(FastestSortingSteps, increasingArray)
 {
     srand(time(nullptr));
-    int size = rand()%1000+1;
+    int size = rand() % 1000 + 1;
 
     int selectionArray[size];
     int insertionArray[size];
     int quickArray[size];
 
     int elements;
-    for (int i = 0; i < size-1; ++i) {
-        elements = rand()%1000+1;
+    for (int i = 0; i < size - 1; ++i) {
+        elements = rand() % 1000 + 1;
 
         selectionArray[i] = elements;
         insertionArray[i] = elements;
@@ -56,7 +56,7 @@ TEST(FastestSortingSteps, increasingArray)
 
     int* newSelectionSort = selectionSort(selectionArray, size, selectionCounter);
     int* newInsertionSort = insertionSort(insertionArray, size, insertionCounter);
-    int* newQuickSort = quickSort(quickArray, 0, size-1, quickCounter);
+    int* newQuickSort = quickSort(quickArray, 0, size - 1, quickCounter);
 
 
     ASSERT_LT(selectionCounter, quickCounter);
@@ -65,15 +65,15 @@ TEST(FastestSortingSteps, increasingArray)
 TEST(FastestSortingSteps, decreasingArray)
 {
     srand(time(nullptr));
-    int size = rand()%1000+1;
+    int size = rand() % 1000 + 1;
 
     int selectionArray[size];
     int insertionArray[size];
     int quickArray[size];
 
     int elements;
-    for (int i = 0; i < size-1; ++i) {
-        elements = rand()%1000+1;
+    for (int i = 0; i < size - 1; ++i) {
+        elements = rand() % 1000 + 1;
 
         selectionArray[i] = elements;
         insertionArray[i] = elements;
@@ -86,7 +86,7 @@ TEST(FastestSortingSteps, decreasingArray)
 
     int* newSelectionSort = selectionSort(selectionArray, size, selectionCounter, 0);
     int* newInsertionSort = insertionSort(insertionArray, size, insertionCounter, 0);
-    int* newQuickSort = quickSort(quickArray, 0, size-1, quickCounter, 0);
+    int* newQuickSort = quickSort(quickArray, 0, size - 1, quickCounter, 0);
 
 
     ASSERT_LT(selectionCounter, quickCounter);
@@ -97,15 +97,15 @@ TEST(FastestSortingTime, increasingArray)
 
 
     srand(time(nullptr));
-    int size = rand()%1000+1;
+    int size = rand() % 1000 + 1;
 
     int selectionArray[size];
     int insertionArray[size];
     int quickArray[size];
 
     int elements;
-    for (int i = 0; i < size-1; ++i) {
-        elements = rand()%1000+1;
+    for (int i = 0; i < size - 1; ++i) {
+        elements = rand() % 1000 + 1;
 
         selectionArray[i] = elements;
         insertionArray[i] = elements;
@@ -121,7 +121,6 @@ TEST(FastestSortingTime, increasingArray)
     auto finish = cv::getTickCount();
     double selectionDuration = (finish - start) / cv::getTickFrequency();
 
-  //  int* newInsertionSort = insertionSort(insertionArray, size, insertionCounter);
     auto start1 = cv::getTickCount();
     int* newQuickSort = quickSort(quickArray, 0, size-1, quickCounter);
     auto finish1 = cv::getTickCount();
@@ -133,14 +132,14 @@ TEST(FastestSortingTime, increasingArray)
 TEST(FastestSortingTime, decreasingVector)
 {
     srand(time(nullptr));
-    int size = rand()%1000+1;
+    int size = rand() % 1000 + 1;
 
     std::vector<int> selectionVector;
     std::vector<int> quickVector;
 
     int elements;
-    for (int i = 0; i < size-1; ++i) {
-        elements = rand()%1000+1;
+    for (int i = 0; i < size - 1; ++i) {
+        elements = rand() % 1000 + 1;
 
         selectionVector.push_back(elements);
         quickVector.push_back(elements);
@@ -165,14 +164,14 @@ TEST(FastestSortingTime, decreasingVector)
 TEST(FastestSortingTime, increasingVector)
 {
     srand(time(nullptr));
-    int size = rand()%1000+1;
+    int size = rand() % 1000 + 1;
 
     std::vector<int> selectionVector;
     std::vector<int> quickVector;
 
     int elements;
-    for (int i = 0; i < size-1; ++i) {
-        elements = rand()%1000+1;
+    for (int i = 0; i < size - 1; ++i) {
+        elements = rand() % 1000 + 1;
 
         selectionVector.push_back(elements);
         quickVector.push_back(elements);
@@ -187,9 +186,103 @@ TEST(FastestSortingTime, increasingVector)
     double selectionDuration = (finish - start) / cv::getTickFrequency();
 
     auto start1 = cv::getTickCount();
-    std::vector<int> newQuickSort = quickSort(quickVector, 0, size-1, quickCounter);
+    std::vector<int> newQuickSort = quickSort(quickVector, 0, size - 1, quickCounter);
     auto finish1 = cv::getTickCount();
     double quickDuration = (finish1 - start1) / cv::getTickFrequency();
 
     ASSERT_LT(selectionDuration, quickDuration);
+}
+TEST(InsertionSortTest, increasingVector)
+{
+    srand(time(nullptr));
+    int size = rand() % 100 + 1;
+    std::vector<int> myVector;
+
+    int elements;
+    for (int i = 0; i < size - 1; ++i) {
+        elements = rand() % 100 + 1;
+        myVector.push_back(elements);
+
+    }
+
+    int counter = 0;
+    std::vector<int> myNumbersInOrder = insertionSort(myVector, static_cast<int>(myVector.size()), counter);
+
+    std::sort(myVector.begin(), myVector.begin()+myVector.size());
+
+    EXPECT_EQ(myNumbersInOrder, myVector);
+}
+
+TEST(InsertionSortTest, decreasingVector)
+{
+    srand(time(nullptr));
+    int size = rand() % 100 + 1;
+    std::vector<int> myVector;
+
+    int elements;
+    for (int i = 0; i < size - 1; ++i) {
+        elements = rand() % 100 + 1;
+        myVector.push_back(elements);
+
+    }
+
+    int counter = 0;
+    std::vector<int> myNumbersInOrder = insertionSort(myVector, static_cast<int>(myVector.size()), counter, 0);
+
+    std::sort(myVector.begin(), myVector.end(), std::greater<>());
+
+    EXPECT_EQ(myNumbersInOrder, myVector);
+
+}
+
+TEST(InsertionSortTest, increasingArray)
+{
+    srand(time(nullptr));
+    int size = rand() % 100 + 1;
+
+    int insertionArray[size];
+    int newInsertionArray[size];
+
+    int elements;
+    for (int i = 0; i < size; ++i) {
+        elements = rand() % 100 + 1;
+        newInsertionArray[i] = elements;
+        insertionArray[i] = elements;
+    }
+
+    int insertionCounter = 0;
+    insertionSort(insertionArray, size, insertionCounter);
+
+    int sizeOfArray = sizeof(newInsertionArray)/ sizeof(newInsertionArray[0]);
+    std::sort(newInsertionArray, newInsertionArray+sizeOfArray);
+
+    for (int k = 0; k < size; ++k) {
+        EXPECT_EQ(insertionArray[k], newInsertionArray[k]);
+    }
+}
+
+TEST(InsertionSortTest, decreasingArray)
+{
+    srand(time(nullptr));
+    int size = rand() % 100 + 1;
+
+    int insertionArray[size];
+    int newInsertionArray[size];
+
+    int elements;
+    for (int i = 0; i < size; ++i) {
+        elements = rand() % 100 + 1;
+        newInsertionArray[i] = elements;
+        insertionArray[i] = elements;
+    }
+
+    int insertionCounter = 0;
+    insertionSort(insertionArray, size, insertionCounter, 0);
+
+    int sizeOfArray = sizeof(newInsertionArray)/ sizeof(newInsertionArray[0]);
+    std::sort(newInsertionArray, newInsertionArray+sizeOfArray, std::greater<>());
+
+    for (int k = 0; k < size; ++k) {
+        EXPECT_EQ(insertionArray[k], newInsertionArray[k]);
+    }
 }
