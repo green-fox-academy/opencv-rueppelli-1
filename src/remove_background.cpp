e81e008fc7fd1a2ee3e56a72ee68e8460a4eaf1e
KisG93 <kisg93@gmail.com>
2019-02-13 15:04:09 +0100

    Create Light Pattern

--- src/remove_background.cpp
+++ src/remove_background.cpp
@@ -21,3 +21,12 @@ void thresholdingInv (cv::Mat image, cv::Mat dst)
     cv::threshold( image, dst, 212, 100, cv::THRESH_BINARY_INV );
     imshow( "Threshold INV", dst );
 }
+
+cv::Mat createLightPattern(cv::Mat image)
+{
+    cv::Mat pattern;
+    cv::blur(image, pattern, cv::Size(image.cols / 3, image.rows / 3));
+    imshow("LightPatternPhoto", pattern);
+
+    return pattern;
+}
