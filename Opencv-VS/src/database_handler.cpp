768ce9b8ba3d00f9e2d49ab79e9c18be78c8afa9
VeghFruzsi <vefruzso88@gmail.com>
2019-03-18 18:22:52 +0100

    Refactor after I. phase

--- Opencv-VS/src/database_handler.cpp
+++ Opencv-VS/src/database_handler.cpp
@@ -6,145 +6,146 @@ int rc;
 char *sql;
 char buffer[100];
 
-static int callBack(void *NotUsed, int argc, char **argv, char **ColName) {
-    int i;
-    for (i = 0; i < argc; i++) {
-        printf("%s = %s\n", ColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-    return 0;
+static int callBack(void *NotUsed, int argc, char **argv, char **ColName) 
+{
+	int i;
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", ColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
 }
 
 int readDataBase(std::string databasePath)
 {
-    unsigned long long int x = databasePath.length();
-    char* charDatabasePath = new char[x + 1];
-    strcpy(charDatabasePath, databasePath.c_str());
+	unsigned long long int x = databasePath.length();
+	char* charDatabasePath = new char[x + 1];
+	strcpy(charDatabasePath, databasePath.c_str());
 
-    rc = sqlite3_open(charDatabasePath, &myDataBase);
+	rc = sqlite3_open(charDatabasePath, &myDataBase);
 
-    if (rc) {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(myDataBase));
-        return (1);
-    } else {
-        fprintf(stderr, "Opened database successfully\n");
-    }
-    return 0;
+	if (rc) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(myDataBase));
+		return (1);
+	}
+	else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	return 0;
 }
 
 int createRecord(std::string databasePath, std::string tableName, std::string path, double processingTime, int detectedCircles)
 {
-    unsigned long long int x = databasePath.length();
-    char* charDatabasePath = new char[x + 1];
-    strcpy(charDatabasePath, databasePath.c_str());
+	unsigned long long int x = databasePath.length();
+	char* charDatabasePath = new char[x + 1];
+	strcpy(charDatabasePath, databasePath.c_str());
 
-    unsigned long long int y = path.length();
-    char* charPath = new char[y + 1];
-    strcpy(charPath, path.c_str());
+	unsigned long long int y = path.length();
+	char* charPath = new char[y + 1];
+	strcpy(charPath, path.c_str());
 
-    unsigned long long int z = tableName.length();
-    char* charTableName = new char[z + 1];
-    strcpy(charTableName, tableName.c_str());
+	unsigned long long int z = tableName.length();
+	char* charTableName = new char[z + 1];
+	strcpy(charTableName, tableName.c_str());
 
-    rc = sqlite3_open(charDatabasePath, &myDataBase);
+	rc = sqlite3_open(charDatabasePath, &myDataBase);
 
-    sprintf(buffer, "INSERT INTO %s(path, processingTimeInSec, detectedCircles) VALUES(\"%s\", %.2f, %d);", charTableName, charPath, processingTime, detectedCircles);
-    
-    rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
+	sprintf(buffer, "INSERT INTO %s(path, processingTimeInSec, detectedCircles) VALUES(\"%s\", %.2f, %d);", charTableName, charPath, processingTime, detectedCircles);
 
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", ErrorMessage);
-        sqlite3_free(ErrorMessage);
-        return 1;
-    } else {
-        fprintf(stdout, "Records created successfully\n");
-        return 0;
-    }
+	rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
+
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", ErrorMessage);
+		sqlite3_free(ErrorMessage);
+		return 1;
+	} else {
+		fprintf(stdout, "Records created successfully\n");
+		return 0;
+	}
 }
 
 int deleteRecord(std::string databasePath, std::string tableName, int id)
 {
-    unsigned long long int x = databasePath.length();
-    char* charDatabasePath = new char[x + 1];
-    strcpy(charDatabasePath, databasePath.c_str());
+	unsigned long long int x = databasePath.length();
+	char* charDatabasePath = new char[x + 1];
+	strcpy(charDatabasePath, databasePath.c_str());
 
-    unsigned long long int z = tableName.length();
-    char* charTableName = new char[z + 1];
-    strcpy(charTableName, tableName.c_str());
+	unsigned long long int z = tableName.length();
+	char* charTableName = new char[z + 1];
+	strcpy(charTableName, tableName.c_str());
 
-    rc = sqlite3_open(charDatabasePath, &myDataBase);
+	rc = sqlite3_open(charDatabasePath, &myDataBase);
 
-    sprintf(buffer, "DELETE FROM %s WHERE _rowid_ = %d;", charTableName, id);
+	sprintf(buffer, "DELETE FROM %s WHERE _rowid_ = %d;", charTableName, id);
 
-    rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
+	rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
 
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", ErrorMessage);
-        sqlite3_free(ErrorMessage);
-        return 1;
-    } else {
-        fprintf(stdout, "Record(s) deleted successfully\n");
-        return 0;
-    }
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", ErrorMessage);
+		sqlite3_free(ErrorMessage);
+		return 1;
+	} else {
+		fprintf(stdout, "Record(s) deleted successfully\n");
+		return 0;
+	}
 }
 
 int updateRecord(std::string databasePath, std::string tableName, std::string userCmdAfterSet)
 {
-    unsigned long long int x = databasePath.length();
-    char* charDatabasePath = new char[x + 1];
-    strcpy(charDatabasePath, databasePath.c_str());
+	unsigned long long int x = databasePath.length();
+	char* charDatabasePath = new char[x + 1];
+	strcpy(charDatabasePath, databasePath.c_str());
 
-    unsigned long long int z = tableName.length();
-    char* charTableName = new char[z + 1];
-    strcpy(charTableName, tableName.c_str());
+	unsigned long long int z = tableName.length();
+	char* charTableName = new char[z + 1];
+	strcpy(charTableName, tableName.c_str());
 
-    unsigned long long int y = userCmdAfterSet.length();
-    char* charUserCmd = new char[y + 1];
-    strcpy(charUserCmd, userCmdAfterSet.c_str());
+	unsigned long long int y = userCmdAfterSet.length();
+	char* charUserCmd = new char[y + 1];
+	strcpy(charUserCmd, userCmdAfterSet.c_str());
 
-    rc = sqlite3_open(charDatabasePath, &myDataBase);
+	rc = sqlite3_open(charDatabasePath, &myDataBase);
 
-    sprintf(buffer, "UPDATE %s SET %s;", charTableName, charUserCmd);
+	sprintf(buffer, "UPDATE %s SET %s;", charTableName, charUserCmd);
 
-    rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
+	rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
 
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", ErrorMessage);
-        sqlite3_free(ErrorMessage);
-        return 1;
-    } else {
-        fprintf(stdout, "Record(s) updated successfully\n");
-        return 0;
-    }
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", ErrorMessage);
+		sqlite3_free(ErrorMessage);
+		return 1;
+	} else {
+		fprintf(stdout, "Record(s) updated successfully\n");
+		return 0;
+	}
 }
 
 int selectRecords(std::string databasePath, std::string tableName, std::string chooseRecord)
 {
+	unsigned long long int x = databasePath.length();
+	char* charDatabasePath = new char[x + 1];
+	strcpy(charDatabasePath, databasePath.c_str());
 
-    unsigned long long int x = databasePath.length();
-    char* charDatabasePath = new char[x + 1];
-    strcpy(charDatabasePath, databasePath.c_str());
+	unsigned long long int z = tableName.length();
+	char* charTableName = new char[z + 1];
+	strcpy(charTableName, tableName.c_str());
 
-    unsigned long long int z = tableName.length();
-    char* charTableName = new char[z + 1];
-    strcpy(charTableName, tableName.c_str());
+	unsigned long long int y = chooseRecord.length();
+	char* charChooseRecord = new char[y + 1];
+	strcpy(charChooseRecord, chooseRecord.c_str());
 
-    unsigned long long int y = chooseRecord.length();
-    char* charChooseRecord = new char[y + 1];
-    strcpy(charChooseRecord, chooseRecord.c_str());
+	rc = sqlite3_open(charDatabasePath, &myDataBase);
 
-    rc = sqlite3_open(charDatabasePath, &myDataBase);
+	sprintf(buffer, "SELECT %s FROM %s;", charChooseRecord, charTableName);
 
-    sprintf(buffer, "SELECT %s FROM %s;", charChooseRecord, charTableName);
+	rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
 
-    rc = sqlite3_exec(myDataBase, buffer, callBack, 0, &ErrorMessage);
-
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", ErrorMessage);
-        sqlite3_free(ErrorMessage);
-        return 1;
-    } else {
-        fprintf(stdout, "Record(s) selected successfully\n");
-        return 0;
-    }
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", ErrorMessage);
+		sqlite3_free(ErrorMessage);
+		return 1;
+	} else {
+		fprintf(stdout, "Record(s) selected successfully\n");
+		return 0;
+	}
 }
